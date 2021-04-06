9c5f9a4961ae797fa9ee738335be9162fd07cbc7
Numai Yuji <26862190+nuuuu@users.noreply.github.com>
2021-04-06 09:34:27 +0900

    house_users indexページにページングを導入

--- fullstack_challenge_1/challenges/nuuuu/app/controllers/house_users_controller.rb
+++ fullstack_challenge_1/challenges/nuuuu/app/controllers/house_users_controller.rb
@@ -1,11 +1,12 @@
 class HouseUsersController < ApplicationController
+  include Pagy::Backend
   def show
     @house_user = HouseUser.find(params[:house_user_id])
 
   end
 
   def index
-    @house_users = HouseUser.all
-
+    house_users = HouseUser.order(id: :asc)
+    @pagy, @house_users = pagy(house_users, items: 20)
   end
 end
