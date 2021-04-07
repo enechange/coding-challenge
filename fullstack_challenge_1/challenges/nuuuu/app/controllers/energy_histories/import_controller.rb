de4fb79b50a81be06f94011856450a26b7a89b60
Numai Yuji <26862190+nuuuu@users.noreply.github.com>
2021-04-07 21:41:15 +0900

    alert brushup

--- fullstack_challenge_1/challenges/nuuuu/app/controllers/energy_histories/import_controller.rb
+++ fullstack_challenge_1/challenges/nuuuu/app/controllers/energy_histories/import_controller.rb
@@ -1,14 +1,18 @@
 module EnergyHistories
   class ImportController < ApplicationController
     def index
-
     end
 
     def create
       uploaded_file = create_params[:file]
+      if uploaded_file.nil?
+        flash[:alert] = "ファイルが参照されていません"
+        return redirect_to action: 'index'
+      end
+
       imported_lines = EnergyHistories::ImportService.new.run(uploaded_file)
 
-      flash[:result] = "#{imported_lines}行のデータをインポートしました。"
+      flash[:info] = "#{imported_lines}行のデータをインポートしました。"
       redirect_to action: 'index'
     end
 
@@ -16,6 +20,8 @@ module EnergyHistories
 
     def create_params
       params.require(:energy_history_file).permit(:file)
+    rescue ActionController::ParameterMissing
+      {}
     end
   end
 end
--- fullstack_challenge_1/challenges/nuuuu/app/controllers/house_users/import_controller.rb
+++ fullstack_challenge_1/challenges/nuuuu/app/controllers/house_users/import_controller.rb
@@ -6,6 +6,11 @@ module HouseUsers
 
     def create
       uploaded_file = create_params[:file]
+      if uploaded_file.nil?
+        flash[:alert] = "ファイルが参照されていません"
+        return redirect_to action: 'index'
+      end
+
       imported_lines = HouseUsers::ImportService.new.run(uploaded_file)
 
       flash[:result] = "#{imported_lines}行のデータをインポートしました。"
@@ -16,6 +21,8 @@ module HouseUsers
 
     def create_params
       params.require(:house_user_file).permit(:file)
+    rescue ActionController::ParameterMissing
+      {}
     end
   end
 end
