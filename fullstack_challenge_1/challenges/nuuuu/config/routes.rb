d069c767a04eb944044df20345eaa1230e50942b
Numai Yuji <26862190+nuuuu@users.noreply.github.com>
2021-04-07 22:12:11 +0900

    トップページを用意

--- fullstack_challenge_1/challenges/nuuuu/config/routes.rb
+++ fullstack_challenge_1/challenges/nuuuu/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'home#index'
+
   resources :house_users, param: :house_user_id, only: %i[index show] do
     collection do
       resources :import , only: %i[index create], controller: 'house_users/import'
