82249ee9df289e67fb2f66dfeaca8f10afd2369b
kenji7157 <kawake.kk@gmail.com>
2021-12-30 21:09:51 +0900

    ヘッダーコンポーネントの実装

---
+++ frontend_challenge_2/challenges/kenji_kawanobe/src/pages/SimulationPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- ヘッダー -->
+    <the-header></the-header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    TheHeader: () => import("@/components/parts/TheHeader.vue"),
+  },
+})
+export default class SimulationPage extends Vue {}
+</script>
