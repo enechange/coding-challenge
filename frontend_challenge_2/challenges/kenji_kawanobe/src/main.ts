7709faa00862cd5af902250ec317210a05fb5f12
kenji7157 <kawake.kk@gmail.com>
2021-12-31 17:35:55 +0900

    シミュレーションデータ(入力値)の型定義を追加

--- frontend_challenge_2/challenges/kenji_kawanobe/src/main.ts
+++ frontend_challenge_2/challenges/kenji_kawanobe/src/main.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
 
 Vue.config.productionTip = false;
 
---
+++ frontend_challenge_2/challenges/kenji_kawanobe/src/types/index.ts
@@ -0,0 +1,40 @@
+/** エリアタイプ */
+export enum areaTypes {
+  TOKYO = "東京エリア",
+  KANSAI = "関西エリア",
+  OTHER = "対象外エリア",
+}
+
+/** 会社 */
+export enum companyTypes {
+  TOKYO_DENRYOKU = "東京電力",
+  KANSAI_DENRYOKU = "関西電力",
+  OTHER = "その他",
+  UNSELECTED = "",
+}
+
+/** プラン */
+export enum planTypes {
+  PLAN_A = "従量電灯A",
+  PLAN_B = "従量電灯B",
+  PLAN_C = "従量電灯C",
+  UNSELECTED = "",
+}
+
+/** シミュレーションデータ(入力) */
+export interface ISimulation {
+  /** 郵便番号(前半) */
+  firstZipCode: string;
+  /** 郵便番号(後半) */
+  secondZipCode: string;
+  /** 会社 */
+  company: companyTypes;
+  /** プラン */
+  plan: planTypes;
+  /** 契約容量 */
+  amps: string;
+  /** 支払金額 */
+  pay: string | number;
+  /** メールアドレス */
+  email: string;
+}
