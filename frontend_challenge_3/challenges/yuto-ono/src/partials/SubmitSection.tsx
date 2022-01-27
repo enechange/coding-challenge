496f0103b067323e5942240ed38c8ec315c3d17f
Yuto Ono <stellarwindmaster@gmail.com>
2022-01-27 09:26:29 +0900

    disabled判定の調整

--- frontend_challenge_3/challenges/yuto-ono/src/partials/SubmitSection.tsx
+++ frontend_challenge_3/challenges/yuto-ono/src/partials/SubmitSection.tsx
@@ -1,13 +1,14 @@
-import { useFormContext } from "react-hook-form"
+import { useFormContext, useWatch } from "react-hook-form"
 import Submit from "../components/Submit"
 import styles from "./SubmitSection.module.scss"
 
 const SubmitSection: React.FC = () => {
   const { formState: { isValid } } = useFormContext()
+  const area = useWatch({ name: "area", defaultValue: "" }) as string
 
   return (
     <div className={styles.section}>
-      <Submit disabled={!isValid}>結果を見る</Submit>
+      <Submit disabled={ !isValid || "" === area }>結果を見る</Submit>
     </div>
   )
 }
