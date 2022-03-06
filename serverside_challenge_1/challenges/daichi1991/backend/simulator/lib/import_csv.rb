0593212ad065f0a20b1cffc7a23085079ad8446c
daichi1991 <ks-pan@live.jp>
2022-03-07 00:02:30 +0900

    CSVをDBにインポートするためのクラスを作成

---
+++ serverside_challenge_1/challenges/daichi1991/backend/simulator/lib/import_csv.rb
@@ -0,0 +1,17 @@
+require "csv"
+
+class ImportCsv
+  def self.execute(model:, file_name: nil)
+    model_name = model.to_s.classify
+    table_name = model_name.tableize
+    file_name ||= table_name.singularize
+    path = Rails.root.join("db/csv_data/#{file_name}.csv")
+
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << row.to_h
+    end
+    model_name.constantize.import!(list, on_duplicate_key_update: :all)
+    ActiveRecord::Base.connection.execute("select setval('#{table_name}_id_seq',(select max(id) from #{table_name}))")
+  end
+end
