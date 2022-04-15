959e8001604b839289fcfbde4ed67e0483a56cf9
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2022-04-15 22:08:39 +0900

    react degrade & npm -> yarn

--- frontend_challenge_3/challenges/ttaniguchi/src/js/index.tsx
+++ frontend_challenge_3/challenges/ttaniguchi/src/js/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 import App from '@/js/components/main';
 
 import '@/css/main.css';
 
-const container = document.getElementById('app') as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
+render(
+  <App />,
+  document.getElementById('app'),
+);
