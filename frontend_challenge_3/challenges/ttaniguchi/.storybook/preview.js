955bb7bf2859751460a5a341d9cbfee1d18ce125
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2022-04-16 00:26:34 +0900

    共通スタイルの設定

--- frontend_challenge_3/challenges/ttaniguchi/.storybook/preview.js
+++ frontend_challenge_3/challenges/ttaniguchi/.storybook/preview.js
@@ -1,9 +1,12 @@
+import '@/css/main.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  layout: 'fullscreen',
+};
