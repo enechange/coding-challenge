f8f2751c7584d9fa4f5d51721054b3caee2865e5
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2022-04-21 10:12:30 +0900

    [bugfix] scssのStorybookビルド対応

--- frontend_challenge_3/challenges/ttaniguchi/.storybook/main.js
+++ frontend_challenge_3/challenges/ttaniguchi/.storybook/main.js
@@ -1,12 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/js/**/*.stories.mdx', '../src/js/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react"
-}
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    });
+
+    return config;
+  },
+};
