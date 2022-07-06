d0ec37f470f9a31679d8c75496bf72034b9d53e5
en-ri <jobenri59@gmail.com>
2022-07-06 10:02:04 +0900

    fix CONTRACT_AMPERAGE CONTRACT_AMPERAGEを複数箇所で使用するため、config/initializers/constants.rbに定義しました。

--- app/models/basic_charge.rb
+++ app/models/basic_charge.rb
@@ -1,11 +1,9 @@
 class BasicCharge < ApplicationRecord
   belongs_to :electricity_rate_plan
 
-  CONTRACT_AMPERAGE = [10, 15, 20, 30, 40, 50, 60]
-
   validates :contract_amperage,
             presence: true,
-            inclusion: { in: CONTRACT_AMPERAGE },
+            inclusion: { in: Constants::CONTRACT_AMPERAGE },
             uniqueness: { scope: :electricity_rate_plan_id }
 
   validates :charge_unit_price,
---
+++ config/initializers/constants.rb
@@ -0,0 +1,4 @@
+module Constants
+  # 契約アンペア数 : 10 / 15 / 20 / 30 / 40 / 50 / 60 のいずれかとなる。
+  CONTRACT_AMPERAGE = [10, 15, 20, 30, 40, 50, 60]
+end
