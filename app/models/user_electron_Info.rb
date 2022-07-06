8c18255ae1edb515e6cc6fff9f7ae2157886a610
en-ri <jobenri59@gmail.com>
2022-07-06 10:03:14 +0900

    add UserElectronInfo requestで受け取った契約アンペア数,電気使用量を保持するclassを作成（validationを使用したいため、modelに定義）

---
+++ app/models/user_electron_Info.rb
@@ -0,0 +1,26 @@
+# requestで受け取った契約アンペア数,電気使用量を保持するclass
+# dbと連携しないmodel（validationを使用したいため、modelに定義）
+class UserElectronInfo
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :contract_amperage, :integer # 契約アンペア数
+  attribute :electricity_usage, :integer # 電気使用量
+
+  validates :contract_amperage,
+            presence: true,
+            inclusion: { in: Constants::CONTRACT_AMPERAGE }
+
+  validates :electricity_usage,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+
+  define_model_callbacks :save, only: :before
+  before_save { throw(:abort) if invalid? }
+
+  def save
+    run_callbacks :save do
+      true
+    end
+  end
+end
