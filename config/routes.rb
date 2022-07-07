fe0f0646bba011f9c0207a9db2a52fd99540cbd0
en-ri <jobenri59@gmail.com>
2022-07-08 05:21:01 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+      resources :electricity_rate_plans, only: :index
+  end
 end
