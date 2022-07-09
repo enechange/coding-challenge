cb2c8ccb2319fc9db4cd10b3b4b0f21dc27fe1f3
en-ri <jobenri59@gmail.com>
2022-07-09 17:00:04 +0900

    add requests_rspec リクエストに含まれる契約アンペア数,電気使用量が不正な値の場合のtestを追加しました。

--- spec/requests/api/electricity_rate_plans_spec.rb
+++ spec/requests/api/electricity_rate_plans_spec.rb
@@ -246,5 +246,111 @@ RSpec.describe "ElectricityRatePlans", type: :request do
         end
       end
     end
+
+    context 'リクエストに含まれる契約アンペア数,電気使用量が不正な値の場合' do
+      context '契約アンペア数が未入力の場合' do
+        it '未入力及び入力条件に関するエラーメッセージを返す' do
+          ampere = ''
+          usage = 400
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "contract_amperage" =>[
+                                                      "未入力です。",
+                                                      "[10, 15, 20, 30, 40, 50, 60]内、いずれかの数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+
+      context '契約アンペア数が文字列の場合' do
+        it '入力条件に関するエラーメッセージを返す' do
+          ampere = 'test'
+          usage = 400
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "contract_amperage" =>[
+                                                      "[10, 15, 20, 30, 40, 50, 60]内、いずれかの数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+
+      context '契約アンペア数が入力条件に該当しない数値の場合' do
+        it '入力条件に関するエラーメッセージを返す' do
+          ampere = 1000
+          usage = 400
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "contract_amperage" =>[
+                                                      "[10, 15, 20, 30, 40, 50, 60]内、いずれかの数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+
+      context '電気使用量が未入力の場合' do
+        it '未入力及び入力条件に関するエラーメッセージを返す' do
+          ampere = 10
+          usage = ''
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "electricity_usage" =>[
+                                                      "未入力です。",
+                                                      "0以上、99999以下の数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+
+      context '電気使用量が文字列の場合' do
+        it '入力条件に関するエラーメッセージを返す' do
+          ampere = 10
+          usage = 'test'
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "electricity_usage" =>[
+                                                      "0以上、99999以下の数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+
+      context '電気使用量が入力条件に該当しない数値の場合' do
+        it '入力条件に関するエラーメッセージを返す' do
+          ampere = 10
+          usage = -1
+
+          get "/api/electricity_rate_plans?contract_amperage=#{ampere}&electricity_usage=#{usage}"
+
+          expect(response.status).to eq(400)
+          expect(JSON.parse(response.body)).to eq({
+                                                    "electricity_usage" =>[
+                                                      "0以上、99999以下の数値を入力してください。"
+                                                    ]
+                                                  }
+                                               )
+        end
+      end
+    end
   end
 end
