8172c7c2fd98012e444b2b5a674e40c5b3746098
en-ri <jobenri59@gmail.com>
2022-07-09 14:40:07 +0900

    fix class ElectricityRateCalculation
    従量料金の計算処理を修正しました。

--- app/services/electricity_rate_calculation.rb
+++ app/services/electricity_rate_calculation.rb
@@ -29,13 +29,21 @@ class ElectricityRateCalculation
     total_charge = 0
     user_usage = user_electron_info.electricity_usage.dup
 
-    target_plan.usage_charges.sort_by { |v| v.minimum_usage }.each do |plan|
-      if plan.max_usage.blank? || user_usage - plan.max_usage <= 0
-        break total_charge += user_usage * plan.charge_unit_price
+    target_plan.usage_charges.sort_by { |v| v.minimum_usage }.each do |plan_usage_charge|
+      # 以下の場合は、処理終了
+      # ・従量料金の区分（○kWh〜○kWhまで）が、全従量料金（target_plan.usage_charges）の中で最大値の場合
+      # ・plan_usage_chargeの区分の最大値がユーザーの電気使用量を超える場合
+      if plan_usage_charge.max_usage.blank? || user_electron_info.electricity_usage < plan_usage_charge.max_usage
+        break total_charge += user_usage * plan_usage_charge.charge_unit_price
       end
 
-      total_charge += plan.max_usage * plan.charge_unit_price
-      user_usage -= plan.max_usage
+      # plan_usage_chargeの従量料金区分の最大値と最小値の差分を取得
+      plan_diff_usage = plan_usage_charge.max_usage - plan_usage_charge.minimum_usage
+
+      # 電気料金の取得
+      total_charge += plan_diff_usage * plan_usage_charge.charge_unit_price
+
+      user_usage -= plan_diff_usage
       break if user_usage.zero?
     end
 
