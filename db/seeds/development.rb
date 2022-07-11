81d4e4b3a907d29162257944bb732becf77c3cb9
en-ri <jobenri59@gmail.com>
2022-07-11 22:59:19 +0900

    fix seed ・不要なメソッドを修正（find_or_initialize_byメソッド） ・test.rbについて、テストの初期データはfixtureを使用した方が良いと思うが、本課題のテストを目的として、seedデータを利用します。

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,27 +1,27 @@
 require "csv"
 
-CSV.foreach('db/csv/development/electric_power_provider.csv', headers: true) do |row|
-  electric_power_provider = ElectricPowerProvider.find_or_initialize_by(name: row['name'])
-  electric_power_provider.save!
+count = 0
+CSV.foreach('db/csv/test/electric_power_provider.csv', headers: true) do |row|
+  ElectricPowerProvider.create!(id: count += 1,
+                                name: row['name'])
 end
 
-CSV.foreach('db/csv/development/electricity_rate_plan.csv', headers: true) do |row|
-  electricity_rate_plan = ElectricityRatePlan.find_or_initialize_by(name: row['name'],
-                                                                    electric_power_provider_id: row['electric_power_provider_id'])
-  electricity_rate_plan.save!
+count = 0
+CSV.foreach('db/csv/test/electricity_rate_plan.csv', headers: true) do |row|
+  ElectricityRatePlan.create!(id: count += 1,
+                              name: row['name'],
+                              electric_power_provider_id: row['electric_power_provider_id'])
 end
 
-CSV.foreach('db/csv/development/basic_charge.csv', headers: true) do |row|
-  base_charge = BasicCharge.find_or_initialize_by(contract_amperage: row['contract_amperage'],
-                                                  electricity_rate_plan_id: row['electricity_rate_plan_id'])
-  base_charge.assign_attributes(charge_unit_price: row['charge_unit_price'])
-  base_charge.save!
+CSV.foreach('db/csv/test/basic_charge.csv', headers: true) do |row|
+  BasicCharge.create!(contract_amperage: row['contract_amperage'],
+                      electricity_rate_plan_id: row['electricity_rate_plan_id'],
+                      charge_unit_price: row['charge_unit_price'])
 end
 
-CSV.foreach('db/csv/development/usage_charge.csv', headers: true) do |row|
-  usage_charge = UsageCharge.find_or_initialize_by(minimum_usage: row['min_usage'],
-                                                   max_usage: row['max_usage'],
-                                                   electricity_rate_plan_id: row['electricity_rate_plan_id'])
-  usage_charge.assign_attributes(charge_unit_price: row['charge_unit_price'])
-  usage_charge.save!
+CSV.foreach('db/csv/test/usage_charge.csv', headers: true) do |row|
+  UsageCharge.create!(charge_unit_price: row['charge_unit_price'],
+                      minimum_usage: row['min_usage'],
+                      max_usage: row['max_usage'],
+                      electricity_rate_plan_id: row['electricity_rate_plan_id'])
 end
