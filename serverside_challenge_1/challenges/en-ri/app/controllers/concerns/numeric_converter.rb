9069dbf4f7f888f656104809d547460c3e727c24
en-ri <jobenri59@gmail.com>
2022-07-16 05:58:52 +0900

    fix module NumericConverter ・レビュー指摘のとおり、関数名と動作が一致していないため、動作ごとにメソッドを分けました。 https://github.com/enechange/coding-challenge/pull/22#discussion_r921727781

--- serverside_challenge_1/challenges/en-ri/app/controllers/concerns/numeric_converter.rb
+++ serverside_challenge_1/challenges/en-ri/app/controllers/concerns/numeric_converter.rb
@@ -3,10 +3,17 @@
 module NumericConverter
   extend ActiveSupport::Concern
 
-  # 文字列が数字（例:1,-1,0.1）の場合、数値に変換する
+  # 文字列が整数（例:1,-1）の場合、整数に変換する
   def convert_integer(str)
-    return str if str.blank? || !str.match?(/^-?\d\.?(\d+)?$/)
+    return str if str.blank? || !str.instance_of?(String) || !str.match?(/^-?\d+$/)
 
-    str.match?(/^-?\d+?$/) ? str.to_i : str.to_f
+    str.to_i
+  end
+
+  # 文字列が小数（例:0.1）の場合、小数に変換する
+  def convert_float(str)
+    return str if str.blank? || !str.instance_of?(String) || !str.match?(/^-?\d+\.\d+$/)
+
+    str.to_f
   end
 end
