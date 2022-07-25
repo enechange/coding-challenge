330b84356b8d72f75e723c1ad724eb73f6c0199a
Masahiro TAKAHASHI <m3takahashi@gmail.com>
2022-07-25 06:41:53 +0000

    Refactoring.

--- serverside_challenge_1/challenges/m1takahashi/test/test_helper.rb
+++ serverside_challenge_1/challenges/m1takahashi/test/test_helper.rb
@@ -7,4 +7,11 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
+  
+  # 電力会社名を元に,APIのレスポンスから指定のプランを取得する
+  def plan_from_response_body(provider_name, body)
+    res = JSON.parse(body)
+    plans = res["data"].select{|item| item["provider_name"] == provider_name }
+    plans.shift
+  end
 end
