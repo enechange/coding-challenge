32fab12c53629af02ec223bdf0154639d5e1f9a3
zizynonno <zizynonno@gmail.com>
2022-08-24 01:45:34 +0900

    add: csv読み込み

--- serverside_challenge_1/challenges/zizynonno/app/models/basic_rate.rb
+++ serverside_challenge_1/challenges/zizynonno/app/models/basic_rate.rb
@@ -1,5 +1,5 @@
 class BasicRate < ApplicationRecord
   belongs_to :electricity_plan
 
-  validates :ampere, inclusion: { in: %w(10 15 20 30 40 50 60), message: "%{value} のサイズは無効です" }
+  validates :ampere, inclusion: { in: [10, 15, 20, 30, 40, 50, 60], message: "%{value} のサイズは無効です" }
 end
