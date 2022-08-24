87deda495481f4cc25e0bbff005d617cc16947b1
zizynonno <zizynonno@gmail.com>
2022-08-24 09:40:57 +0900

    change: 金額計算部分をメソッドに切り出し

--- serverside_challenge_1/challenges/zizynonno/app/models/electricity_rate.rb
+++ serverside_challenge_1/challenges/zizynonno/app/models/electricity_rate.rb
@@ -12,19 +12,22 @@ class ElectricityRate
     electric_power_companies = ElectricPowerCompany.includes(electricity_plans: :basic_rate).includes(electricity_plans: :meter_rate)
     electric_power_companies.all.each do |company|
       company.electricity_plans.each do |plan|
-        basic = plan.basic_rate.find { |basic| basic.ampere == @ampere }
-        meter = plan.meter_rate.where(max_usage: nil).find { |meter| meter.min_usage < @usage }
-        if meter.present?
-          price = basic.price + meter.price * @usage
-          responce.push({ provider_name: meter.electricity_plan.electric_power_company.name, plan_name: plan.name, price: price })
+        basic_rate = plan.basic_rate.find { |basic| basic.ampere == @ampere }
+        meter_rate = plan.meter_rate.where(max_usage: nil).find { |meter| meter.min_usage < @usage }
+        if meter_rate.present?
+          responce.push({ provider_name: meter_rate.electricity_plan.electric_power_company.name, plan_name: plan.name, price: calculate_total(basic_rate, meter_rate) })
         else
-          next if basic.blank?
-          meter = plan.meter_rate.where.not(max_usage: nil).find{ |meter| meter.min_usage < @usage && meter.max_usage >= @usage }
-          price = basic.price + meter.price * @usage
-          responce.push({ provider_name: meter.electricity_plan.electric_power_company.name, plan_name: plan.name, price: price })
+          next if basic_rate.blank?
+          meter_rate = plan.meter_rate.where.not(max_usage: nil).find{ |meter| meter.min_usage < @usage && meter.max_usage >= @usage }
+          responce.push({ provider_name: meter_rate.electricity_plan.electric_power_company.name, plan_name: plan.name, price: calculate_total(basic_rate, meter_rate) })
         end
       end
     end
     responce
   end
+
+  private
+    def calculate_total(basic_rate, meter_rate)
+      basic_rate.price + meter_rate.price * @usage
+    end
 end
