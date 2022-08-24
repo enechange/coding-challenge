5e07812b197547d562c3c4b6242378f415e97fb5
zizynonno <zizynonno@gmail.com>
2022-08-24 09:16:08 +0900

    change: validate箇所を切り出す

--- serverside_challenge_1/challenges/zizynonno/app/controllers/api/v1/electricity_plans_controller.rb
+++ serverside_challenge_1/challenges/zizynonno/app/controllers/api/v1/electricity_plans_controller.rb
@@ -1,16 +1,9 @@
 module Api
   module V1
     class ElectricityPlansController < ApplicationController
-      def index
-        responce = params[:ampere].to_i + params[:usage].to_i
-        electricity_plan = ElectricityPlan.find(1)
-        base_rate = electricity_plan.basic_rate.build(ampere: params[:ampere].to_i, price: 1000)
-        meter_rate = electricity_plan.meter_rate.build(min_usage: params[:usage].to_i, max_usage: 1000, price: 1000)
-        return render json: { status: 'failure', data: base_rate.errors.full_messages } if base_rate.invalid?
-        return render json: { status: 'failure', data: meter_rate.errors.full_messages } if meter_rate.invalid?
+      before_action :params_validate, only: [:index]
 
-        # 電力会社でループ回す
-        # includesする
+      def index
         responce = []
         electric_power_companies = ElectricPowerCompany.includes(electricity_plans: :basic_rate).includes(electricity_plans: :meter_rate)
         electric_power_companies.all.each do |company|
@@ -37,6 +30,15 @@ module Api
             :ampere,
             :usage
           )
+        end
+
+        def params_validate
+          responce = params[:ampere].to_i + params[:usage].to_i
+          electricity_plan = ElectricityPlan.find(1)
+          base_rate = electricity_plan.basic_rate.build(ampere: params[:ampere].to_i, price: 1000)
+          meter_rate = electricity_plan.meter_rate.build(min_usage: params[:usage].to_i, max_usage: 1000, price: 1000)
+          return render json: { status: 'failure', data: base_rate.errors.full_messages } if base_rate.invalid?
+          return render json: { status: 'failure', data: meter_rate.errors.full_messages } if meter_rate.invalid?
         end
     end
   end
