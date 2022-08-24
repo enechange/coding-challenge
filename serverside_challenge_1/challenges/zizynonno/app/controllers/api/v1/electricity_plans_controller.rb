daf1ba7dadfd6be993ea796fc358c6e8ba629a52
zizynonno <zizynonno@gmail.com>
2022-08-24 09:03:59 +0900

    add: 電力会社、プラン、料金のjson形式出力機能を追加

--- serverside_challenge_1/challenges/zizynonno/app/controllers/api/v1/electricity_plans_controller.rb
+++ serverside_challenge_1/challenges/zizynonno/app/controllers/api/v1/electricity_plans_controller.rb
@@ -9,6 +9,22 @@ module Api
         return render json: { status: 'failure', data: base_rate.errors.full_messages } if base_rate.invalid?
         return render json: { status: 'failure', data: meter_rate.errors.full_messages } if meter_rate.invalid?
 
+        # 電力会社でループ回す
+        # includesする
+        responce = []
+        electric_power_companies = ElectricPowerCompany.includes(electricity_plans: :basic_rate).includes(electricity_plans: :meter_rate)
+        electric_power_companies.all.each do |company|
+          company.electricity_plans.each do |plan|
+            plan.basic_rate.find { |basic| basic.ampere == params[:ampere].to_i }
+            meter = plan.meter_rate.where(max_usage: nil).find { |meter| meter.min_usage < params[:usage].to_i }
+            if meter.present?
+              responce.push({ provider_name: meter.electricity_plan.electric_power_company.name, plan_name: plan.name, price: 1000 })
+            else
+               meter = plan.meter_rate.where.not(max_usage: nil).find{ |meter| meter.min_usage < params[:usage].to_i && meter.max_usage >= params[:usage].to_i }
+               responce.push({ provider_name: meter.electricity_plan.electric_power_company.name, plan_name: plan.name, price: 1000 })
+            end
+          end
+        end
         render json: { status: 'success', data: responce }
       end
 
