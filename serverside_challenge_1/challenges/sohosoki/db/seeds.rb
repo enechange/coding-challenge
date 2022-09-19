4aef943b79d6677c3972fe70f9317c480e928991
So Hosoki <fucso.h@gmail.com>
2022-09-19 21:29:06 +0900

    初期データ登録時に登録済みの電力会社はスキップ

--- serverside_challenge_1/challenges/sohosoki/db/seeds.rb
+++ serverside_challenge_1/challenges/sohosoki/db/seeds.rb
@@ -6,7 +6,14 @@ data['providers'].each do |provider_data|
   provider = Provider.create(name: provider_data['name'])
   provider_data['plans'].each do |plan_data|
 
-    plan = Plan.create(provider: provider, name: plan_data['name'])
+    # 同じ生で登録済みならスキップ
+    name = plan_data['name']
+    if Plan.find_by(name: name).present?
+      puts "skip #{name}"
+      next
+    end
+
+    plan = Plan.create(provider: provider, name: name)
     plan_data['basic_charges'].each do |charge_data|
       charge_data['plan'] = plan
       BasicCharge.create(charge_data)
