07827c9e0230494005b9c589a7d58b36c95086ac
So Hosoki <fucso.h@gmail.com>
2022-09-19 22:04:12 +0900

    不要な api クローズ

--- serverside_challenge_1/challenges/sohosoki/config/routes.rb
+++ serverside_challenge_1/challenges/sohosoki/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :providers, only: [:index]
       resources :plans, only: [] do
         collection do
           get 'simulate'
