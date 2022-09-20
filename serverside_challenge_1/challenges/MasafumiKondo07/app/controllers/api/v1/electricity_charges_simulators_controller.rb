82630c7dacb50761411286cbb8db200422922e04
近藤真史 <kondo.iffromlife.tkhr@gmail.com>
2022-09-20 16:19:54 +0900

    プランごとの電気料金を返すAPIの作成

---
+++ serverside_challenge_1/challenges/MasafumiKondo07/app/controllers/api/v1/electricity_charges_simulators_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class ElectricityChargesSimulatorsController < ApplicationController
+      def index
+        companies = Company.all
+        simulation_list = []
+        companies.each do |company|
+          company.plans.each do |plan|
+            basic_charge_instance = plan.basic_charges.find_by(ampere: params[:ampere])
+            next if basic_charge_instance.nil?
+            electricity_fee_instance = plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: params[:amount_used]..)
+            electricity_fee_instance = plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: nil) if electricity_fee_instance.nil?
+            
+            simulation_list << {provider_name: company.name, plan_name: plan.name, price: calc_result(basic_charge_instance.price, electricity_fee(electricity_fee_instance.price))}
+          end
+        end
+        # 指定された基本料金でのプランが見つからない場合、エラーメッセージを返却
+        if (simulation_list.blank?)
+          render json: error_object
+          return
+        end
+        render json: simulation_list
+      end
+
+      private
+
+      def calc_result(basic_charge, electricity_fee)
+        basic_charge + electricity_fee
+      end
+
+      def electricity_fee(unit_price)
+        unit_price * params[:amount_used].to_i
+      end
+
+      def error_object
+        {
+          message: "指定されたアンペア数での料金プランは見つかりませんでした。"
+        }
+      end
+    end
+  end
+end
