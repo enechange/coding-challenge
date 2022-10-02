9b7a2c7b410bb5478c8b4a6ae13a265ff14664ea
近藤真史 <kondo.iffromlife.tkhr@gmail.com>
2022-10-02 21:31:33 +0900

    nil判定を後置ifから「 ||=」に変更

--- serverside_challenge_1/challenges/MasafumiKondo07/app/controllers/api/v1/electricity_charges_simulators_controller.rb
+++ serverside_challenge_1/challenges/MasafumiKondo07/app/controllers/api/v1/electricity_charges_simulators_controller.rb
@@ -9,7 +9,7 @@ module Api
             basic_charge_instance = plan.basic_charges.find_by(ampere: params[:ampere])
             next if basic_charge_instance.nil?
             electricity_fee_instance = plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: params[:amount_used]..)
-            electricity_fee_instance = plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: nil) if electricity_fee_instance.nil?
+            electricity_fee_instance ||= plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: nil)
             
             simulation_list << {provider_name: company.name, plan_name: plan.name, price: calc_result(basic_charge_instance.price, electricity_fee_instance.calc(params[:amount_used].to_i))}
           end
