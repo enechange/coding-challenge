ac8e8e6a32c5abdc8ad687cb7d99ff3659df3bb9
近藤真史 <kondo.iffromlife.tkhr@gmail.com>
2022-10-04 08:50:10 +0900

    従量料金の計算方法の修正

--- serverside_challenge_1/challenges/MasafumiKondo07/app/models/electricity_fee.rb
+++ serverside_challenge_1/challenges/MasafumiKondo07/app/models/electricity_fee.rb
@@ -2,6 +2,14 @@ class ElectricityFee < ApplicationRecord
   belongs_to :plan
 
   def calc(amount_used)
-    price * amount_used
+    target_electricity_fees = self.class.where(plan_id: plan_id).order(classification_min: :asc)
+    result = 0
+    target_electricity_fees.each do |instance|
+      break if (amount_used < instance.classification_min)
+      usage_lower_limit = instance.classification_min == 0 ? 0 : instance.classification_min - 1
+      usage_upper_limit = instance.classification_max.present? && instance.classification_max < amount_used ? instance.classification_max : amount_used
+      result += (usage_upper_limit - usage_lower_limit) * instance.price
+    end
+    result
   end
 end
