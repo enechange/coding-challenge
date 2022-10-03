152a7678f4a92ddb6e955a8c905eb17af43244b3
近藤真史 <kondo.iffromlife.tkhr@gmail.com>
2022-10-04 08:55:32 +0900

    電気料金算出ロジックをPlanモデルに寄せる

--- serverside_challenge_1/challenges/MasafumiKondo07/app/controllers/api/v1/electricity_charges_simulators_controller.rb
+++ serverside_challenge_1/challenges/MasafumiKondo07/app/controllers/api/v1/electricity_charges_simulators_controller.rb
@@ -11,7 +11,7 @@ module Api
             electricity_fee_instance = plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: params[:amount_used]..)
             electricity_fee_instance ||= plan.electricity_fees.find_by(classification_min: ..params[:amount_used], classification_max: nil)
             
-            simulation_list << {provider_name: company.name, plan_name: plan.name, price: calc_result(basic_charge_instance.price, electricity_fee_instance.calc(params[:amount_used].to_i))}
+            simulation_list << {provider_name: company.name, plan_name: plan.name, price: plan.applicable_plan_fee(basic_charge_instance.price, electricity_fee_instance.calc(params[:amount_used].to_i))}
           end
         end
         # 指定された基本料金でのプランが見つからない場合、エラーメッセージを返却
@@ -23,11 +23,6 @@ module Api
       end
 
       private
-
-      def calc_result(basic_charge, electricity_fee)
-        basic_charge + electricity_fee
-      end
-
       def error_object
         {
           message: "指定されたアンペア数での料金プランは見つかりませんでした。"
--- serverside_challenge_1/challenges/MasafumiKondo07/app/models/plan.rb
+++ serverside_challenge_1/challenges/MasafumiKondo07/app/models/plan.rb
@@ -2,4 +2,8 @@ class Plan < ApplicationRecord
   belongs_to :company
   has_many :electricity_fees, dependent: :destroy
   has_many :basic_charges, dependent: :destroy
+
+  def applicable_plan_fee(basic_charge, electricity_fee)
+    basic_charge + electricity_fee
+  end
 end
