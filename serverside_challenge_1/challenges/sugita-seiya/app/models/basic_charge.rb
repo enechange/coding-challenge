85fa8d52402cdac4693840def86c88b37d0b558b
sugita-seiya <satreu16@ezweb.ne.jp>
2022-10-27 08:06:01 +0900

    feat: 基本料金プランモデルのバリデーションを追加

--- serverside_challenge_1/challenges/sugita-seiya/app/models/basic_charge.rb
+++ serverside_challenge_1/challenges/sugita-seiya/app/models/basic_charge.rb
@@ -1,3 +1,15 @@
 class BasicCharge < ApplicationRecord
   belongs_to :electricity_rate_plan
+
+  validates :contract_amperage,
+            presence: true,
+            inclusion: { in: Constants::CONTRACT_AMPERAGE_TYPE },
+            uniqueness: { scope: :electricity_rate_plan_id, message: 'プラン、契約アンペア数の組み合わせは存在します' }
+
+  validates :charge_unit_price,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+
+  validates :electricity_rate_plan_id,
+            presence: true
 end
