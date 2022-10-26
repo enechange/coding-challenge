ba2d14a9e8a934d4306f5c2b6dee079a8e033d65
sugita-seiya <satreu16@ezweb.ne.jp>
2022-10-27 08:03:03 +0900

    feat: 電気料金プランモデルのバリデーションを追加

--- serverside_challenge_1/challenges/sugita-seiya/app/models/electricity_rate_plan.rb
+++ serverside_challenge_1/challenges/sugita-seiya/app/models/electricity_rate_plan.rb
@@ -2,4 +2,10 @@ class ElectricityRatePlan < ApplicationRecord
   belongs_to :electric_power_provider
   has_many :basic_charges, dependent: :destroy
   has_many :usage_charges, dependent: :destroy
+
+  validates :name,
+            presence: true,
+            uniqueness: { scope: :electric_power_provider_id, message: '電力会社名、プラン名の組み合わせは存在します。' }
+
+  validates :electric_power_provider_id, presence: true
 end
