b66653bd9d9825879b94c43adbcd0417618f9b69
sugita-seiya <satreu16@ezweb.ne.jp>
2022-10-27 08:20:19 +0900

    feat: 従量料金モデルのバリデーションを追加

--- serverside_challenge_1/challenges/sugita-seiya/app/models/usage_charge.rb
+++ serverside_challenge_1/challenges/sugita-seiya/app/models/usage_charge.rb
@@ -1,3 +1,27 @@
 class UsageCharge < ApplicationRecord
   belongs_to :electricity_rate_plan
+
+  validates :charge_unit_price,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+
+  validates :minimum_usage,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+            uniqueness: { scope: %i[electricity_rate_plan_id max_usage], message: 'プラン、区分（最大値、最小値）の組み合わせは存在します。' }
+
+  validates :electricity_rate_plan_id,
+            presence: true
+
+  validate :check_minimum_is_not_over_maximum
+
+  def check_minimum_is_not_over_maximum
+    if minimum_usage.blank? || max_usage.blank?
+      return
+    end
+
+    if max_usage <= minimum_usage
+      errors.add(:minimum_usage, '最大値未満の数値を入力してください。')
+    end
+  end
 end
