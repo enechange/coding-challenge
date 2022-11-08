a86af89c69ce17f32dc5babf6c46af9123ba9229
sugita-seiya <satreu16@ezweb.ne.jp>
2022-11-08 18:17:19 +0900

    refactor: コード量を減らすために三項演算子を使用

--- serverside_challenge_1/challenges/sugita-seiya/app/controllers/concerns/numeric_converter.rb
+++ serverside_challenge_1/challenges/sugita-seiya/app/controllers/concerns/numeric_converter.rb
@@ -2,10 +2,6 @@ module NumericConverter
   extend ActiveSupport::Concern
 
   def convert_integer(str)
-    if str.blank? || !str.match?(/^\d+$/)
-      return str
-    end
-
-    str.to_i
+    return str.blank? || !str.match?(/^\d+$/) ? str : str.to_i
   end
 end
