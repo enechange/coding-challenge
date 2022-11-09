2a7a9adda0de092c654afde76e61a60c88c3512e
sugita-seiya <satreu16@ezweb.ne.jp>
2022-11-09 18:23:52 +0900

    refactor: returnがなくても返却できるためreturnを削除

--- serverside_challenge_1/challenges/sugita-seiya/app/controllers/concerns/numeric_converter.rb
+++ serverside_challenge_1/challenges/sugita-seiya/app/controllers/concerns/numeric_converter.rb
@@ -2,6 +2,6 @@ module NumericConverter
   extend ActiveSupport::Concern
 
   def convert_integer(str)
-    return str.blank? || !str.match?(/^\d+$/) ? str : str.to_i
+    str.blank? || !str.match?(/^\d+$/) ? str : str.to_i
   end
 end
