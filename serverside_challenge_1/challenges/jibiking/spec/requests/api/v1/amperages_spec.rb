ffe0161f9e3a50718496061aabf0a9af27b25545
jibiki <jibiki@jibikinoMacBook-Pro.local>
2022-11-11 13:30:28 +0900

    create: amperage,kilowatto spec作成OK

---
+++ serverside_challenge_1/challenges/jibiking/spec/requests/api/v1/amperages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'support/import_helper'
+
+RSpec.describe 'Amperages', type: :request do
+	describe 'POST /api/v1/amperages' do
+    let(:path){'/api/v1/amperages'}
+    let(:success_file){'amperages.csv'}
+    let(:error_file){'kilowattos.csv'}
+
+    describe '成功' do
+      it_behaves_like '正しい形式のファイルのインポートに成功すること'
+    end
+
+    describe '失敗' do
+      it_behaves_like '誤った形式のファイルのインポートに失敗すること'
+    end
+  end
+end
---
+++ serverside_challenge_1/challenges/jibiking/spec/requests/api/v1/kilowattos_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'support/import_helper'
+
+RSpec.describe 'Kilowattos', type: :request do
+	describe 'POST /api/v1/kilowattos' do
+    let(:path){'/api/v1/kilowattos'}
+    let(:success_file){'kilowattos.csv'}
+    let(:error_file){'amperages.csv'}
+
+    describe '成功' do
+      it_behaves_like '正しい形式のファイルのインポートに成功すること'
+    end
+
+    describe '失敗' do
+      it_behaves_like '誤った形式のファイルのインポートに失敗すること'
+    end
+  end
+end
