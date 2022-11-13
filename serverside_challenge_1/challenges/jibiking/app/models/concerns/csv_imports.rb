30d1b4f2099cdae3aba25e05e48391af0585099c
jibiki <jibiki@jibikinoMacBook-Pro.local>
2022-11-13 16:50:25 +0900

    refactor: import時、csvでidを明記しなくともimport可能に

--- serverside_challenge_1/challenges/jibiking/app/models/concerns/csv_imports.rb
+++ serverside_challenge_1/challenges/jibiking/app/models/concerns/csv_imports.rb
@@ -4,12 +4,18 @@ module CsvImports
   module ClassMethods
     def import(file)
       import_error_count = 0
+      row_count = 0
 
       # CSVファイルのインポート
       CSV.foreach(file.path, headers: true) do |row|
+        
+        # idを付与
+        row_count += 1
+        row["id"] = row_count
+
         # rowのカラムがupdatable_atributesと同じ場合のみ処理を実行
-        row_keys = row.to_h.keys
-        if row_keys == updatable_attributes
+        row_keys = row.to_h.keys.sort
+        if row_keys == updatable_attributes.sort
           record = find_by(id: row["id"]) || new
           record.attributes = row.to_hash.slice(*updatable_attributes)
           record.save
