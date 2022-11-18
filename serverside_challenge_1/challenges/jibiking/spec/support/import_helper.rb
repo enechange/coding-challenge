d3ce6a14217cd32424289656844dc2f596d4ca9c
jibiki <jibiki@jibikinoMacBook-Pro.local>
2022-11-18 14:02:09 +0900

    refactor: インデント修正

--- serverside_challenge_1/challenges/jibiking/spec/support/import_helper.rb
+++ serverside_challenge_1/challenges/jibiking/spec/support/import_helper.rb
@@ -1,38 +1,38 @@
 shared_examples '正しい形式のファイルを指定した場合' do
-	it 'インポートに成功する' do
-		post path, params: {
-			file: fixture_file_upload(success_file)
-		}
+  it 'インポートに成功する' do
+    post path, params: {
+      file: fixture_file_upload(success_file)
+    }
 
-		expect(response.status).to eq(200)
-		expect(body).to eq({
-			"Success"=>"インポートが成功しました。"
-	})
-	end
+    expect(response.status).to eq(200)
+    expect(body).to eq({
+      "Success"=>"インポートが成功しました。"
+  })
+  end
 end
 
 shared_examples '誤った形式のファイルを指定した場合' do
-	it 'インポートに失敗する' do
-		post path, params: {
-			file: fixture_file_upload(error_file)
-		}
+  it 'インポートに失敗する' do
+    post path, params: {
+      file: fixture_file_upload(error_file)
+    }
 
-		expect(response.status).to eq(400)
-		expect(body).to eq({
-			"Erorr"=>"インポートが失敗しました。CSVファイルのデータ形式を見直してください。"
-	})
-	end
+    expect(response.status).to eq(400)
+    expect(body).to eq({
+      "Erorr"=>"インポートが失敗しました。CSVファイルのデータ形式を見直してください。"
+  })
+  end
 end
 
 shared_examples 'ファイルを指定しなかった場合' do
-	it 'インポートに失敗する' do
-		post path, params: {
-			file: ''
-		}
+  it 'インポートに失敗する' do
+    post path, params: {
+      file: ''
+    }
 
-		expect(response.status).to eq(400)
-		expect(body).to eq({
-			"Erorr"=>"インポートが失敗しました。CSVファイルを指定してください。"
-		})
-	end
+    expect(response.status).to eq(400)
+    expect(body).to eq({
+      "Erorr"=>"インポートが失敗しました。CSVファイルを指定してください。"
+    })
+  end
 end
