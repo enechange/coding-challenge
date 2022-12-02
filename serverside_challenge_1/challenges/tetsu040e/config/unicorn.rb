24673d829b9c3e2a91d937fdca5ff307582d204d
Toru SASAKI <tetsu@moove.co.jp>
2022-12-02 16:56:17 +0900

    change port 3000 -> 9000

--- serverside_challenge_1/challenges/tetsu040e/config/unicorn.rb
+++ serverside_challenge_1/challenges/tetsu040e/config/unicorn.rb
@@ -5,7 +5,7 @@ worker_processes  2
 timeout           15
 working_directory rails_root
 pid               File.expand_path 'tmp/pids/unicorn.pid', rails_root
-listen            3000
+listen            9000
 stdout_path       File.expand_path 'log/unicorn.log', rails_root
 stderr_path       File.expand_path 'log/unicorn.log', rails_root
 preload_app       true
