e1b9b74fa16365f24e232d1f5cfe93a936d5badd
kenji7157 <kawake.kk@gmail.com>
2023-02-11 14:12:18 +0900

    サーバーサイドチャレンジでは、フロントエンドの課題を同期する（空にする）

--- frontend_challenge_2/challenges/kenji_kawanobe/.stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: "stylelint-config-standard-scss",
-  rules: {
-    // NOTE: クラス名指定のルールを緩和
-    "selector-class-pattern": null,
-  },
-};
--- frontend_challenge_2/challenges/kenji_kawanobe/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript",
-  testMatch: ["<rootDir>/src/**/*.spec.(ts|tsx)"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-};
