afe70cecc75f21af3f0c7c89cd5946782960e906
ysk91 <ysk91.engineer@gmail.com>
2023-03-30 21:30:17 +0900

    costs_controllerを作成

---
+++ serverside_challenge_1/challenges/yourname/app/controllers/api/v1/costs_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::CostsController < ApplicationController
+  require 'yaml'
+
+  def index
+    yaml_path = params[:path] # 例: 'config/my_yaml.yml'
+    if File.exist?(yaml_path)
+      yaml_data = YAML.load_file(yaml_path)
+      render json: {
+        status: 'SUCCESS',
+        message: '電力会社とコスト一覧の取得に成功しました',
+        data: yaml_data
+      }, status: 200
+    else
+      render json: { error: 'File not found' }, status: 404
+    end
+  end
+
+  # def culculate
+
+  # end
+end
