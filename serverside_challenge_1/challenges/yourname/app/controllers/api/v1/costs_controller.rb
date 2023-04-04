798e0c5ae3f9d48b9429300a9e04f7d295f3f2b0
ysk91 <ysk91.engineer@gmail.com>
2023-04-04 15:43:25 +0900

    存在しない契約アンペアの処理を追加

--- serverside_challenge_1/challenges/yourname/app/controllers/api/v1/costs_controller.rb
+++ serverside_challenge_1/challenges/yourname/app/controllers/api/v1/costs_controller.rb
@@ -22,19 +22,28 @@ class Api::V1::CostsController < ApplicationController
       costs = []
       rates.keys.each do |key|
         basic_rate = rates[key.to_s]['basic_rates'][contract_ampere]
-        # usage_rate = rates[key.to_s]['usage_rates']
-        usage_rate = nil
+        next unless basic_rate
+
+        usage_total_cost = 0
+        remaining_usage = usage
+
         rates[key.to_s]['usage_rates'].each do |rate_info|
           min, max = rate_info['range'].map { |value| value == "inf" ? Float::INFINITY : value }
-          if min <= usage && usage <= max
-            usage_rate = rate_info['rate']
+
+          if remaining_usage >= max
+            usage_total_cost += rate_info['rate'] * (max - min + 1)
+            remaining_usage -= (max - min + 1)
+          else
+            usage_total_cost += rate_info['rate'] * remaining_usage
+            remaining_usage -= remaining_usage
             break
           end
         end
-        total_cost = basic_rate + (usage_rate * usage)
+        total_cost = basic_rate + usage_total_cost
 
-        costs << { 電力会社: key,
-                    料金: total_cost
+        costs << { provider_name: key,
+                    plan_name: rates[key.to_s]['plan_name'],
+                    price: total_cost
                   }
       end
       render json: costs.to_json, status: 200
