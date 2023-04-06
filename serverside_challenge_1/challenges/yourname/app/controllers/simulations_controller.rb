aab3300d907318e1e6192d739794c9af96cedec3
ysk91 <ysk91.engineer@gmail.com>
2023-04-06 14:54:22 +0900

    入力画面を作成

---
+++ serverside_challenge_1/challenges/yourname/app/controllers/simulations_controller.rb
@@ -0,0 +1,5 @@
+class SimulationsController < ActionController::Base
+  def index
+    ampere_selection = [10, 15, 20, 30, 40, 50, 60]
+  end
+end
--- serverside_challenge_1/challenges/yourname/config/routes.rb
+++ serverside_challenge_1/challenges/yourname/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :simulations, only: [:index]
+
   namespace :api do
     namespace :v1 do
       resources :costs, only: [:index] do
