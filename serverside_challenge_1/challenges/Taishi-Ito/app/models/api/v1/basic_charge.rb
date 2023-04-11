b24d2c0937329223ffded9d89ada96ab0b4dd2b2
Taishi-Ito <taishi.ito1026@gmail.com>
2023-04-11 09:31:20 +0900

    fix:Fix associated class name

--- serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/basic_charge.rb
+++ serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/basic_charge.rb
@@ -1,3 +1,3 @@
 class Api::V1::BasicCharge < ApplicationRecord
-  belongs_to :api_v1_plan
+  belongs_to :api_v1_plan, :class_name => 'Api::V1::Plan'
 end
--- serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
+++ serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
@@ -1,5 +1,5 @@
 class Api::V1::Plan < ApplicationRecord
-  belongs_to :api_v1_company
-  has_many :api_v1_basic_charges, dependent: :destroy
-  has_many :api_v1_usage_charges, dependent: :destroy
+  belongs_to :api_v1_company, :class_name => 'Api::V1::Company'
+  has_many :api_v1_basic_charges, dependent: :destroy, :class_name => 'Api::V1::BasicCharge', foreign_key: 'api_v1_plan_id'
+  has_many :api_v1_usage_charges, dependent: :destroy, :class_name => 'Api::V1::UsageCharge', foreign_key: 'api_v1_plan_id'
 end
