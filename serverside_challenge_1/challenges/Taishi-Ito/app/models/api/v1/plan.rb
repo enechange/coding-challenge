a8e161e7933f3db0b44364b7aea8b1e880b20266
Taishi-Ito <taishi.ito1026@gmail.com>
2023-04-11 17:36:57 +0900

    feat:Add function to calculate charge to plan model

--- serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
+++ serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
@@ -2,4 +2,15 @@ class Api::V1::Plan < ApplicationRecord
   belongs_to :api_v1_company, :class_name => 'Api::V1::Company'
   has_many :api_v1_basic_charges, dependent: :destroy, :class_name => 'Api::V1::BasicCharge', foreign_key: 'api_v1_plan_id'
   has_many :api_v1_usage_charges, dependent: :destroy, :class_name => 'Api::V1::UsageCharge', foreign_key: 'api_v1_plan_id'
+
+  def basic_charge ampere
+    basic_charge = self.api_v1_basic_charges.find_by(ampere: ampere).charge
+    return basic_charge
+  end
+
+  def usage_charge kwh
+    usage_record = self.api_v1_usage_charges.where('from_khw < ? AND to_khw > ?', kwh, kwh)
+    usage_charge = (usage_record.first.charge * kwh) + usage_record.first.stacked_charge
+    return usage_charge
+  end
 end
