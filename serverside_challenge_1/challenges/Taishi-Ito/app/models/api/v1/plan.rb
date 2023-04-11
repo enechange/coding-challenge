55d0b4d3274c89b4e5ec9333c20eef4b227e1cbf
Taishi-Ito <taishi.ito1026@gmail.com>
2023-04-11 23:46:12 +0900

    fix:Fix plan model

--- serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
+++ serverside_challenge_1/challenges/Taishi-Ito/app/models/api/v1/plan.rb
@@ -4,13 +4,18 @@ class Api::V1::Plan < ApplicationRecord
   has_many :api_v1_usage_charges, dependent: :destroy, :class_name => 'Api::V1::UsageCharge', foreign_key: 'api_v1_plan_id'
 
   def basic_charge ampere
-    basic_charge = self.api_v1_basic_charges.find_by(ampere: ampere).charge
-    return basic_charge
+    basic_charge = self.api_v1_basic_charges.find_by(ampere: ampere)
+    return basic_charge ? basic_charge.charge : nil
   end
 
   def usage_charge kwh
-    usage_record = self.api_v1_usage_charges.where('from_khw < ? AND to_khw > ?', kwh, kwh)
-    usage_charge = (usage_record.first.charge * kwh) + usage_record.first.stacked_charge
+    return 0.00 if kwh < 1
+    usage_record = self.api_v1_usage_charges.where('? >= from_khw AND ? <= to_khw', kwh, kwh)
+    unless usage_record.size > 0
+      usage_charge = nil
+    else
+      usage_charge = (usage_record.first.charge * kwh) + usage_record.first.stacked_charge
+    end
     return usage_charge
   end
 end
