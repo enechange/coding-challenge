1cce842764e42420b31598697b60bca673d4fd4c
ysk91 <ysk91.engineer@gmail.com>
2023-04-11 10:48:37 +0900

    outputアクションでhttpsリクエストを送るように設定

--- serverside_challenge_1/challenges/sonoki/app/controllers/simulations_controller.rb
+++ serverside_challenge_1/challenges/sonoki/app/controllers/simulations_controller.rb
@@ -12,7 +12,7 @@ class SimulationsController < ActionController::Base
     uri = URI("#{ENV['API_URI']}/api/v1/costs/calculate_rate")
     uri.query = URI.encode_www_form({ contract_ampere: contract_ampere, usage: usage })
     request = Net::HTTP::Get.new(uri)
-    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
 
