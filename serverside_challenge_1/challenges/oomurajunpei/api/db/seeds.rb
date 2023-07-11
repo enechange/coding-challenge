9841005adfd20cd700241a63a956f299df7b7ba8
大村純平 <junpei.aug07@gmail.com>
2023-07-11 17:48:28 +0900

    csvファイルの追加と読み込み処理の追加

--- serverside_challenge_1/challenges/oomurajunpei/api/db/seeds.rb
+++ serverside_challenge_1/challenges/oomurajunpei/api/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('./app/assets/provider.csv', headers: true) do |r|
+  Provider.where(name: r['name'], plan_name: r['plan_name']).first_or_create!(
+    name: r['name'],
+    plan_name: r['plan_name']
+  )
+end
+
+CSV.foreach('./app/assets/basic_rate.csv', headers: true) do |r|
+  BasicRate.where(provider_id: r['provider_id'], ampere: r['ampere'], price: r['price']).first_or_create!(
+    provider_id: r['provider_id'],
+    ampere: r['ampere'],
+    price: r['price']
+  )
+end
+
+CSV.foreach('./app/assets/pay_per_use_rate.csv', headers: true) do |r|
+  PayPerUseRate.where(provider_id: r['provider_id'], unit_price: r['unit_price'], min_electricity_usage: r['min_electricity_usage'], max_electricity_usage: r['max_electricity_usage']).first_or_create!(
+    provider_id: r['provider_id'],
+    unit_price: r['unit_price'],
+    min_electricity_usage: r['min_electricity_usage'],
+    max_electricity_usage: r['max_electricity_usage']
+  )
+end
