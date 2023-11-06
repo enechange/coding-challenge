b231a1f8c0a3df0eb93e515c060ce7dedeb75249
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-11-07 02:56:26 +0900

    chore: create-next-app

---
+++ frontend_challenge_2/challenges/toru_shimizu/coding-challenge/.jsconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  compilerOptions: {
+    jsx: "react",
+    baseUrl: "src",
+  },
+};
---
+++ frontend_challenge_2/challenges/toru_shimizu/coding-challenge/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
+    return config;
+  },
+}
+
+module.exports = nextConfig
