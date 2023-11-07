72930163ebe56c22fceba22986c89b2ea572ca36
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-11-07 11:32:29 +0900

    feat: Selectコンポーネントを追加

---
+++ frontend_challenge_2/challenges/toru_shimizu/coding-challenge/src/components/ui/select.tsx
@@ -0,0 +1,49 @@
+import { forwardRef, type ComponentPropsWithoutRef } from "react";
+import { Label } from "./label";
+import styles from "./select.module.scss";
+
+interface Props {
+  attention?: string;
+  name: string;
+  options: string[];
+  label: string;
+  error?: string;
+}
+
+type ChildProps = ComponentPropsWithoutRef<"select"> & Props;
+
+export const Select = forwardRef<HTMLSelectElement, ChildProps>(
+  ({ attention, label, error, options, ...props }, ref) => {
+    const existsOptions = options.length > 0;
+
+    return (
+      <div className={styles.selectWrapper}>
+        <Label name={props.name}>{label}</Label>
+        <select
+          id={props.name}
+          className={`${styles.select} ${
+            !existsOptions ? styles.disabled : ""
+          }`}
+          {...props}
+          ref={ref}
+        >
+          {existsOptions ? (
+            <>
+              <option value="">{label}を選択してください</option>
+              {options.map((option) => (
+                <option key={option}>{option}</option>
+              ))}
+            </>
+          ) : (
+            <option value="">選択できる{label}がありません</option>
+          )}
+        </select>
+        {!existsOptions && attention !== "" && (
+          <p className={styles.attention}>{attention}</p>
+        )}
+      </div>
+    );
+  },
+);
+
+Select.displayName = "Select";
