e147075c205c612624e1e224924664ac70384d6f
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-18 22:14:01 +0900

    Refactor header component tests

--- frontend_challenge_2/challenges/k-taro56/tests/components/header.test.tsx
+++ frontend_challenge_2/challenges/k-taro56/tests/components/header.test.tsx
@@ -1,29 +1,29 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-// import Header from '../../src/components/header';
+import Header from '../../src/components/header';
 
-// describe('Header component', () => {
-//   it('renders title and subtitle correctly', () => {
-//     const title = 'Test Title';
-//     const subTitle = 'Test Subtitle';
+describe('Header component', () => {
+  it('renders title and subtitle correctly', () => {
+    const title = 'Test Title';
+    const subTitle = 'Test Subtitle';
 
-//     render(<Header title={title} subTitle={subTitle} />);
+    render(<Header title={title} subTitle={subTitle} />);
 
-//     expect(screen.getByText(title)).toBeInTheDocument();
-//     expect(screen.getByText(subTitle)).toBeInTheDocument();
-//   });
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(subTitle)).toBeInTheDocument();
+  });
 
-//   it('renders multiline text correctly', () => {
-//     const title = 'Line1\\nLine2';
-//     const subTitle = 'LineA\\nLineB';
+  it('renders multiline text correctly', () => {
+    const title = 'Line1\\nLine2';
+    const subTitle = 'LineA\\nLineB';
 
-//     render(<Header title={title} subTitle={subTitle} />);
+    render(<Header title={title} subTitle={subTitle} />);
 
-//     expect(screen.getByText('Line1')).toBeInTheDocument();
-//     expect(screen.getByText('Line2')).toBeInTheDocument();
-//     expect(screen.getByText('LineA')).toBeInTheDocument();
-//     expect(screen.getByText('LineB')).toBeInTheDocument();
-//   });
-// });
+    expect(screen.getByText('Line1')).toBeInTheDocument();
+    expect(screen.getByText('Line2')).toBeInTheDocument();
+    expect(screen.getByText('LineA')).toBeInTheDocument();
+    expect(screen.getByText('LineB')).toBeInTheDocument();
+  });
+});
