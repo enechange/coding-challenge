f5e6121163b497987ab7af539108abe0ee102e43
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-18 22:05:36 +0900

    add header components test

---
+++ frontend_challenge_2/challenges/k-taro56/tests/components/header.test.tsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+
+// import Header from '../../src/components/header';
+
+// describe('Header component', () => {
+//   it('renders title and subtitle correctly', () => {
+//     const title = 'Test Title';
+//     const subTitle = 'Test Subtitle';
+
+//     render(<Header title={title} subTitle={subTitle} />);
+
+//     expect(screen.getByText(title)).toBeInTheDocument();
+//     expect(screen.getByText(subTitle)).toBeInTheDocument();
+//   });
+
+//   it('renders multiline text correctly', () => {
+//     const title = 'Line1\\nLine2';
+//     const subTitle = 'LineA\\nLineB';
+
+//     render(<Header title={title} subTitle={subTitle} />);
+
+//     expect(screen.getByText('Line1')).toBeInTheDocument();
+//     expect(screen.getByText('Line2')).toBeInTheDocument();
+//     expect(screen.getByText('LineA')).toBeInTheDocument();
+//     expect(screen.getByText('LineB')).toBeInTheDocument();
+//   });
+// });
