70878cec59b4422ae21bf2accd2be948061558d1
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2023-11-21 03:26:39 +0900

    Add expand icon

--- frontend_challenge_2/challenges/k-taro56/src/components/form-components/selection.tsx
+++ frontend_challenge_2/challenges/k-taro56/src/components/form-components/selection.tsx
@@ -4,10 +4,33 @@ import styled from '@emotion/styled';
 import { Light, Dark } from '@/colors/theme';
 import FormBase from './form-base';
 
+const SelectWrapper = styled.div`
+  position: relative;
+  &::before {
+    content: 'expand_more';
+    font-family: var(--font-material-symbols);
+    font-size: 1.75rem;
+    color: ${Light.accent};
+    position: absolute;
+    left: 0.5rem;
+    top: 50%;
+    transform: translateY(-50%);
+    pointer-events: none;
+    @media (prefers-color-scheme: dark) {
+      color: ${Dark.accent};
+    }
+  }
+`;
+
 const SelectionInput = styled.select`
   border: 0.25rem solid #ddd;
-  border-radius: 0.25rem;
-  padding: 0.75rem;
+  border-top-left-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  padding-left: 2.25rem;
+  padding-right: 0.75rem;
+  width: 100%;
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
@@ -54,20 +77,22 @@ const SelectionForm = ({
       isVisible={isVisible}
       errorMessage={errorMessage}
     >
-      <SelectionInput
-        required={required}
-        value={selected}
-        onChange={handleSelectionChange}
-      >
-        <option value='' disabled>
-          選択してください
-        </option>
-        {selections.map((selection) => (
-          <option key={selection} value={selection}>
-            {selection}
+      <SelectWrapper>
+        <SelectionInput
+          required={required}
+          value={selected}
+          onChange={handleSelectionChange}
+        >
+          <option value='' disabled>
+            選択してください
           </option>
-        ))}
-      </SelectionInput>
+          {selections.map((selection) => (
+            <option key={selection} value={selection}>
+              {selection}
+            </option>
+          ))}
+        </SelectionInput>
+      </SelectWrapper>
     </FormBase>
   );
 };
