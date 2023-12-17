df90fd29adec4e29ec7094f03ff58c87e7796342
RyuichiUeda <hsatlefp@gmail.com>
2023-12-17 20:24:32 +0900

    不要な細分化をまとめた

--- serverside_challenge_1/challenges/ryuichi_ueda/app/services/initialize_charge_plan.rb
+++ serverside_challenge_1/challenges/ryuichi_ueda/app/services/initialize_charge_plan.rb
@@ -5,35 +5,19 @@ class InitializeChargePlan
 
   attr_reader :basic_charges, :tiers, :provider, :plan
 
-  @providers_data_cache = nil
-
-  def initialize(provider, _plan)
-    @providers_data = self.class.load_providers
-    prepared_data = prepare_provider_data(provider)
-    @basic_charges = prepared_data[:basic_charges]
-    @tiers = prepared_data[:tiers]
-    @plan = prepared_data[:plan]
+  def initialize(provider, plan)
+    @provider_data = self.class.load_providers[provider]
+    @basic_charges = generate_basic_charges
+    @tiers = generate_tiers
+    @plan = plan || @provider_data['plan']
     @provider = provider
   end
-
-  private
 
   def self.load_providers
     @load_providers ||= YAML.load_file(YAML_PATH)['providers']
   end
 
-  def prepare_provider_data(provider)
-    load_provider(provider)
-    {
-      basic_charges: generate_basic_charges,
-      tiers: generate_tiers,
-      plan: generate_plan
-    }
-  end
-
-  def load_provider(provider)
-    @provider_data = @providers_data[provider]
-  end
+  private
 
   def generate_basic_charges
     VALID_AMPERES.index_with do |ampere|
@@ -43,11 +27,7 @@ class InitializeChargePlan
 
   def generate_tiers
     @provider_data['tiers'].transform_keys do |key|
-      ['Infinity'].include?(key) ? Float::INFINITY : key
+      key == 'Infinity' ? Float::INFINITY : key
     end.freeze
-  end
-
-  def generate_plan
-    @provider_data['plan']
   end
 end
