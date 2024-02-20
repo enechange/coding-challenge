9cc1f3cf40c2f7e34c54b95e6d3feed1c53d5b0f
Neil <mitsuru.jack@gmail.com>
2024-02-20 23:57:59 +0900

    Add const file

---
+++ serverside_challenge_2/challenges/jack20xx/config/initializers/constants.rb
@@ -0,0 +1,6 @@
+AMP_NUMBERS = [10, 15, 20, 30, 40, 50, 60].freeze
+ELECTRICITY_CHARGES_PATH = 'config/electricity_charges_list/*.yml'.freeze
+
+# Error messages
+INVALID_NUMBER_ERROR = '値は数字で入力してください。小数点や記号は使用できません。'.freeze
+INVALID_AMP_ERROR = "'正しいアンペア数(#{AMP_NUMBERS.join(', ')})を入力してください'".freeze
