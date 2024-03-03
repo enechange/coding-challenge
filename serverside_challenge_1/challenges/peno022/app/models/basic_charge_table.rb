a490294700dbe8225967c713afa661fc92e6c7a4
peno022 <40317050+peno022@users.noreply.github.com>
2024-03-03 22:53:56 +0900

    コメントの追加

--- serverside_challenge_1/challenges/peno022/app/models/basic_charge_table.rb
+++ serverside_challenge_1/challenges/peno022/app/models/basic_charge_table.rb
@@ -7,6 +7,7 @@ class BasicChargeTable
     @unit_prices = unit_prices
   end
 
+  # 基本料金を計算する。単位は銭。（100銭 = 1円）
   def calculate_charge(contract_amperage)
     unit_prices[contract_amperage]
   end
--- serverside_challenge_1/challenges/peno022/app/models/energy_use_charge_table.rb
+++ serverside_challenge_1/challenges/peno022/app/models/energy_use_charge_table.rb
@@ -7,6 +7,9 @@ class EnergyUseChargeTable
     @unit_prices = unit_prices.sort_by { |unit_price| unit_price.range.begin }
   end
 
+  # 従量料金を計算する。単位は銭。（100銭 = 1円）
+  # 従量料金は消費電力量の区分ごとに単価が異なるため、消費した電力量に応じて料金を計算する。
+  # 例） 0〜120kwhまでは1kwhあたり19円88銭、121〜300kwhまでは1kwhあたり26円48銭、301kwh以上は1kwhあたり30円57銭
   def calculate_charge(consumption)
     total_charge = 0
     last_range_max = 0
