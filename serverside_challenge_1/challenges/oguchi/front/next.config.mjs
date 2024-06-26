c15ed54c8b60adf55a7b740a7c341d8b6ad1b40d
Kohei Oguchi <173341518+k-oguch1@users.noreply.github.com>
2024-06-26 12:23:33 +0900

    add frontend Dockerfile

--- serverside_challenge_1/challenges/oguchi/front/next.config.mjs
+++ serverside_challenge_1/challenges/oguchi/front/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
 export default nextConfig;
