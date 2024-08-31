2d3b82ddf6053baee7522a08f7ab81e08b1f3d46
akihiro.nakano <nakano1akihiro@gmail.com>
2024-09-01 01:50:18 +0900

    [fix]: validator がちゃんと動作していなかったため、修正しました 　→ 単純にエラー文言を渡す動作がおかしかったため

--- serverside_challenge_2/challenge/app/models/validator/period_validator.rb
+++ serverside_challenge_2/challenge/app/models/validator/period_validator.rb
@@ -3,11 +3,11 @@
 module Validator
   class PeriodValidator < ActiveModel::Validator
     def validate(record)
-      if record.from.present? && [record.to, record.from].compact.max == record.to
-        record.errors[:name] << '開始値が、終了値よりも小さくなっています'
-      elsif record.from.present? && ElectricityUsage.exists?(from: record.from.., to: ..record.to,
+      if record.from.present? && [record.to, record.from].compact.max == record.from
+        record.errors.add(:base, '終了値が、開始値よりも小さくなっています')
+      elsif record.from.present? && ElectricityUsage.exists?(from: ..record.from, to: record.to..,
                                                              plan_id: record.plan_id)
-        record.errors[:name] << '同一プランで重複した値が登録されています'
+        record.errors.add(:base, '同一プランで重複した値が登録されています')
       end
     end
   end
