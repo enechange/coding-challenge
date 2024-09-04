dd5d70ea815f5c7a2672be220045a84415186aa1
akihiro.nakano <nakano1akihiro@gmail.com>
2024-09-04 23:52:39 +0900

    [fix]: 条件に誤りがあったため、修正しました　 　→ そもそも必要なパラメータを渡していなかったことを修正 　→ MAX が未設定の場合は上限なしで設定されているため、その場合に被っていないかを調べるときに範囲内に開始の値がちゃんと含まれているかどうかを調べるようにしました

--- serverside_challenge_2/challenge/app/validators/period_validator.rb
+++ serverside_challenge_2/challenge/app/validators/period_validator.rb
@@ -3,7 +3,7 @@
 class PeriodValidator < ActiveModel::Validator
   def validate(record)
     if record.to.blank?
-      record.errors.add(:base, '同一プランで重複した値が登録されています') if end_infinite?
+      record.errors.add(:base, '同一プランで重複した値が登録されています') if end_infinite?(record)
     elsif [record.to, record.from].compact.max == record.from
       record.errors.add(:base, '終了値が、開始値よりも小さくなっています')
     elsif ElectricityUsage.exists?(from: ..record.from, to: record.to..,
@@ -13,7 +13,7 @@ class PeriodValidator < ActiveModel::Validator
   end
 
   def end_infinite?(record)
-    ElectricityUsage.where(from: record.from.., to: ..record.from, plan_id: record.plan_id)
+    ElectricityUsage.where(from: ..record.from, to: record.from.., plan_id: record.plan_id)
                     .or(ElectricityUsage.where(
                           from: record.from.., to: nil, plan_id: record.plan_id
                         )).present?
