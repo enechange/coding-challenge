a4bb7d9ef0bfccacbec4cc7bd8eb37aff27713d7
akihiro.nakano <nakano1akihiro@gmail.com>
2024-09-04 23:22:29 +0900

    [fix]: 最大値が nil の場合に、落ちてしまうため落ちないようにしつつ、validator を変更してみました 　→ 終端とした場合のパターンは問題なさそうなので、こちらの対応にしてます。（かなり複雑になってしまったので、もう少し見直したい...）

--- serverside_challenge_2/challenge/app/validators/period_validator.rb
+++ serverside_challenge_2/challenge/app/validators/period_validator.rb
@@ -2,10 +2,16 @@
 
 class PeriodValidator < ActiveModel::Validator
   def validate(record)
-    if record.from.present? && [record.to, record.from].compact.max == record.from
+    if record.to.blank?
+      # 終端値が未設定の場合
+      if ElectricityUsage.exists?(from: record.from.., plan_id: record.plan_id) ||
+        ElectricityUsage.exists?(to: record.from.., plan_id: record.plan_id)
+        record.errors.add(:base, '同一プランで重複した値が登録されています')
+      end
+    elsif [record.to, record.from].compact.max == record.from
       record.errors.add(:base, '終了値が、開始値よりも小さくなっています')
-    elsif record.from.present? && ElectricityUsage.exists?(from: ..record.from, to: record.to..,
-                                                           plan_id: record.plan_id)
+    elsif ElectricityUsage.exists?(from: ..record.from, to: record.to..,
+                                   plan_id: record.plan_id)
       record.errors.add(:base, '同一プランで重複した値が登録されています')
     end
   end
