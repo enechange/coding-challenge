7666c4757f3d5f9465af59080637c298272ae1bd
akihiro.nakano <nakano1akihiro@gmail.com>
2024-09-04 23:36:22 +0900

    [fix]: rubocop に怒られていたので、メソッド分けを実施しました 　そして、いまいち分かりづらいメソッドになっていたため修正して、範囲が正しく取得されていない箇所を修正しました

--- serverside_challenge_2/challenge/app/validators/period_validator.rb
+++ serverside_challenge_2/challenge/app/validators/period_validator.rb
@@ -3,16 +3,19 @@
 class PeriodValidator < ActiveModel::Validator
   def validate(record)
     if record.to.blank?
-      # 終端値が未設定の場合
-      if ElectricityUsage.exists?(from: record.from.., plan_id: record.plan_id) ||
-        ElectricityUsage.exists?(to: record.from.., plan_id: record.plan_id)
-        record.errors.add(:base, '同一プランで重複した値が登録されています')
-      end
+      record.errors.add(:base, '同一プランで重複した値が登録されています') if end_infinite?
     elsif [record.to, record.from].compact.max == record.from
       record.errors.add(:base, '終了値が、開始値よりも小さくなっています')
     elsif ElectricityUsage.exists?(from: ..record.from, to: record.to..,
                                    plan_id: record.plan_id)
       record.errors.add(:base, '同一プランで重複した値が登録されています')
     end
+  end
+
+  def end_infinite?(record)
+    ElectricityUsage.where(from: record.from.., to: ..record.from, plan_id: record.plan_id)
+                    .or(ElectricityUsage.where(
+                          from: record.from.., to: nil, plan_id: record.plan_id
+                        )).present?
   end
 end
