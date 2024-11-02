f56dc8f135b967f992f34ef9bb0f7114fb6629ea
kuni <kuni@kuninoMac-mini.local>
2024-11-02 15:19:12 +0900

    Seedデータの投入対応

--- serverside_challenge_2/challenge/db/seeds.rb
+++ serverside_challenge_2/challenge/db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('db/seeds/basic_fee.csv'), headers: true) do |row|
+  company = ElectricPowerCompany.find_or_create_by(name: row[0])
+  plan = company.plans.find_or_create_by(name: row[1])
+  # データ投入
+  plan.basic_prices.delete_all
+  plan.basic_prices.create!(amperage: row[2], price: row[3])
+end
+
+CSV.foreach(Rails.root.join('db/seeds/measured_rates.csv'), headers: true) do |row|
+  company = ElectricPowerCompany.find_or_create_by(name: row[0])
+  plan = company.plans.find_or_create_by(name: row[1])
+  electricity_usages = row[2].split('-')
+  # データ投入
+  plan.measured_rates.delete_all
+  plan.measured_rates.create!(electricity_usage_min: electricity_usages[0].to_i,
+                               electricity_usage_max: electricity_usages[1]&.to_i,
+                               price: row[3])
+end
