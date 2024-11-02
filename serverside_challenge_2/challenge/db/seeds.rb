8048f64b329a5a608bcaf3052550e72cac1cea31
kuni <kuni@kuninoMac-mini.local>
2024-11-02 15:58:11 +0900

    CSVデータ投入の妥当性検証の追加

--- serverside_challenge_2/challenge/db/seeds.rb
+++ serverside_challenge_2/challenge/db/seeds.rb
@@ -1,19 +1,19 @@
 require 'csv'
 
-CSV.foreach(Rails.root.join('db/seeds/basic_fee.csv'), headers: true) do |row|
+BasicPrice.delete_all
+CSV.foreach(Rails.root.join('db/seeds/basic_prices.csv'), headers: true) do |row|
   company = ElectricPowerCompany.find_or_create_by(name: row[0])
   plan = company.plans.find_or_create_by(name: row[1])
   # データ投入
-  plan.basic_prices.delete_all
   plan.basic_prices.create!(amperage: row[2], price: row[3])
 end
 
+MeasuredRate.delete_all
 CSV.foreach(Rails.root.join('db/seeds/measured_rates.csv'), headers: true) do |row|
   company = ElectricPowerCompany.find_or_create_by(name: row[0])
   plan = company.plans.find_or_create_by(name: row[1])
   electricity_usages = row[2].split('-')
   # データ投入
-  plan.measured_rates.delete_all
   plan.measured_rates.create!(electricity_usage_min: electricity_usages[0].to_i,
                                electricity_usage_max: electricity_usages[1]&.to_i,
                                price: row[3])
---
+++ serverside_challenge_2/challenge/spec/db/seeds_spec.rb
@@ -0,0 +1,49 @@
+describe 'Seedデータ作成', type: :helper do
+  let(:basic_prices_csv_path) { Rails.root.join('db/seeds/basic_prices.csv') }
+  let(:measured_rates_csv_path) { Rails.root.join('db/seeds/measured_rates.csv') }
+
+  before do
+    Rails.application.load_seed
+  end
+
+  it 'ElectricPowerCompanyが作成されること' do
+    CSV.foreach(basic_prices_csv_path, headers: true) do |row|
+      company = ElectricPowerCompany.find_by!(name: row[0])
+      expect(company).to be_present
+    end
+  end
+
+  it 'Planが作成されること' do
+    CSV.foreach(basic_prices_csv_path, headers: true) do |row|
+      plan = Plan.find_by!(name: row[1])
+      expect(plan).to be_present
+
+      expect(plan.electric_power_company.name).to eq row[0]
+    end
+  end
+
+  it '基本料金が作成されること' do
+    CSV.foreach(basic_prices_csv_path, headers: true) do |row|
+      plan = Plan.find_by!(name: row[1])
+      basic_price = BasicPrice.find_by!(amperage: row[2], plan: plan)
+      expect(basic_price).to be_present
+
+      expect(basic_price.price).to eq row[3].to_f
+    end
+  end
+
+  it '従量料金が作成されること' do
+    CSV.foreach(measured_rates_csv_path, headers: true) do |row|
+      plan = Plan.find_by!(name: row[1])
+
+      usage = row[2].split('-').map(&:to_i)
+      electricity_usage_min = usage[0]
+      electricity_usage_max = usage[1] || MeasuredRate::MAX_SMALL_INT_VALUE
+      measured_rate = MeasuredRate.find_by!(electricity_usage_min: electricity_usage_min,
+                                            electricity_usage_max: electricity_usage_max,
+                                            plan: plan)
+      expect(measured_rate).to be_present
+      expect(measured_rate.price).to eq row[3].to_f
+    end
+  end
+end
