2f38a6f06999d8caf8d063479dc71d0b60f969f6
kuni <kuni@kuninoMac-mini.local>
2024-11-02 11:19:56 +0900

    Rubocopによる自動修正

--- serverside_challenge_2/challenge/config/application.rb
+++ serverside_challenge_2/challenge/config/application.rb
@@ -12,7 +12,7 @@ module App
     config.load_defaults 7.0
 
     # Timezoneの設定
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
