bb28fbe6d049caa7a416da3fd99664bc94ff28f8
kuni <kuni@kuninoMac-mini.local>
2024-11-02 10:40:04 +0900

    Railsのtimezone設定の追加

--- serverside_challenge_2/challenge/config/application.rb
+++ serverside_challenge_2/challenge/config/application.rb
@@ -11,6 +11,10 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Timezoneの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
