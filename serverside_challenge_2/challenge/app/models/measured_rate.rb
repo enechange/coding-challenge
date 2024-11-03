374c00c991e078154a497f85dfa6e49e6254123a
kuni <kuni@kuninoMac-mini.local>
2024-11-03 11:29:02 +0900

    従量料金の計算methodの追加

--- serverside_challenge_2/challenge/app/models/measured_rate.rb
+++ serverside_challenge_2/challenge/app/models/measured_rate.rb
@@ -16,7 +16,38 @@ class MeasuredRate < ApplicationRecord
     super(value.nil? ? MAX_SMALL_INT_VALUE : value)
   end
 
+  class << self
+    def calc_prices(electricity_usage_kwh)
+      rows = MeasuredRate.where("electricity_usage_min <= ?", electricity_usage_kwh).includes(:plan)
+      rows.inject({}) do |sum, row|
+        sum[row.plan_id] ||= { price: 0 }
+        sum[row.plan_id][:price] += row.calc_row_price(electricity_usage_kwh)
+        sum
+      end
+    end
+
+    def validate_electricity_usage?(electricity_usage)
+      res = { is_error: !electricity_usage.is_a?(Integer) }
+      res[:error_object] = { field: "electricity_usage_kwh", message: "整数を指定してください。" } if res[:is_error]
+      res
+    end
+  end
+
+  def calc_row_price(electricity_usage_kwh)
+    return 0 if electricity_usage_kwh.to_i.zero?
+
+    min = self.electricity_usage_min_for_calc
+    max = self.electricity_usage_max <= electricity_usage_kwh ?
+            self.electricity_usage_max :
+            electricity_usage_kwh
+    (max - min) * self.price
+  end
+
   private
+
+  def electricity_usage_min_for_calc
+    self.electricity_usage_min.zero? ? 0 : self.electricity_usage_min - 1
+  end
 
   def validate_max_greater_than_min
     return if electricity_usage_max.nil? || electricity_usage_min.nil?
--- serverside_challenge_2/challenge/spec/models/measure_rate_spec.rb
+++ serverside_challenge_2/challenge/spec/models/measure_rate_spec.rb
@@ -245,4 +245,209 @@ RSpec.describe MeasuredRate, type: :model do
       end
     end
   end
+
+  describe 'Methods' do
+    let(:provider1) { create(:provider, name: 'provider1') }
+    let(:provider2) { create(:provider, name: 'provider2') }
+    let(:plan1_provider1) { create(:plan, name: 'plan1', provider: provider1) }
+    let(:plan2_provider1) { create(:plan, name: 'plan2', provider: provider1) }
+    let(:plan3_provider2) { create(:plan, name: 'plan3', provider: provider2) }
+
+    describe 'validate_electricity_usage?' do
+      it '整数の場合エラーではないこと' do
+        res = MeasuredRate.validate_electricity_usage?(1)
+        expect(res[:is_error]).to be_falsey
+        expect(res[:error_object]).to be_nil
+      end
+
+      it 'nilの場合エラーであること' do
+        res = MeasuredRate.validate_electricity_usage?(nil)
+        expect(res[:is_error]).to be_truthy
+        expect(res[:error_object][:field]).to eq 'electricity_usage_kwh'
+        expect(res[:error_object][:message]).to eq "整数を指定してください。"
+      end
+
+      it '実数の場合エラーであること' do
+        res = MeasuredRate.validate_electricity_usage?(1.1)
+        expect(res[:is_error]).to be_truthy
+        expect(res[:error_object][:field]).to eq 'electricity_usage_kwh'
+        expect(res[:error_object][:message]).to eq "整数を指定してください。"
+      end
+    end
+
+    describe 'calc_row_price' do
+      context 'instance.electricity_usage_min == 0の場合' do
+        let(:measured_rate) { create(:measured_rate, plan: plan, electricity_usage_min: 0, electricity_usage_max: 10, price: 20.01) }
+        before do
+          measured_rate
+        end
+
+        it 'instance.electricity_usage_max < 引数の場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(11)).to eq 200.10
+        end
+
+        it '引数 == instance.electricity_usage_maxの場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(10)).to eq 200.10
+        end
+
+        it '引数 < instance.electricity_usage_maxの場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(9)).to eq 180.09
+        end
+
+        it '引数 == 1の場合、0となる' do
+          expect(measured_rate.calc_row_price(1)).to eq 20.01
+        end
+
+        it '引数 == 0の場合、0となる' do
+          expect(measured_rate.calc_row_price(0)).to eq 0
+        end
+      end
+
+      context 'instance.electricity_usage_min > 0の場合' do
+        let(:measured_rate) { create(:measured_rate, plan: plan, electricity_usage_min: 121, electricity_usage_max: 300, price: 10.01) }
+        before do
+          measured_rate
+        end
+
+        it 'instance.electricity_usage_max < 引数の場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(301)).to eq 1801.8
+        end
+
+        it '引数 == instance.electricity_usage_maxの場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(300)).to eq 1801.8
+        end
+
+        it '引数 < instance.electricity_usage_maxの場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(299)).to eq 1791.79
+        end
+
+        it '引数 == instance.electricity_usage_minの場合、価格計算が妥当であること' do
+          expect(measured_rate.calc_row_price(121)).to eq 10.01
+        end
+
+        it '引数 == 0の場合、0となる' do
+          expect(measured_rate.calc_row_price(0)).to eq 0
+        end
+
+        it '引数 == nilの場合、0となる' do
+          expect(measured_rate.calc_row_price(nil)).to eq 0
+        end
+      end
+    end
+
+    describe 'electricity_usage_min_for_calc' do
+      it 'electricity_usage_minが0の場合0を返す' do
+        instance = create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 0, electricity_usage_max: 10)
+        expect(instance.send(:electricity_usage_min_for_calc)).to eq 0
+      end
+
+      it 'electricity_usage_minが0以外の場合electricity_usage_min - 1を返す' do
+        instance = create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 121, electricity_usage_max: 300)
+        expect(instance.send(:electricity_usage_min_for_calc)).to eq 120
+      end
+    end
+
+    describe 'calc_prices' do
+      # plan1
+      let(:plan1_rate_0_120) { create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 0, electricity_usage_max: 120, price: 19.88) }
+      let(:plan1_rate_121_300) { create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 121, electricity_usage_max: 300, price: 26.48) }
+      let(:plan1_rate_301) { create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 301, electricity_usage_max: nil, price: 30.57) }
+      # plan2
+      let(:plan2_rate_0_120) { create(:measured_rate, plan: plan2_provider1, electricity_usage_min: 0, electricity_usage_max: 120, price: 10.0) }
+      let(:plan2_rate_121_300) { create(:measured_rate, plan: plan2_provider1, electricity_usage_min: 121, electricity_usage_max: 300, price: 20.0) }
+      let(:plan2_rate_301) { create(:measured_rate, plan: plan2_provider1, electricity_usage_min: 301, electricity_usage_max: nil, price: 30.0) }
+      # plan3
+      let(:plan3_rate_0) { create(:measured_rate, plan: plan3_provider2, electricity_usage_min: 0, electricity_usage_max: nil, price: 10.0) }
+
+      context '無段階の価格表の場合' do
+        before do
+          plan3_rate_0
+        end
+
+        it '価格計算が妥当であること' do
+          res = MeasuredRate.calc_prices(1000)
+
+          expect(res.size).to eq 1
+          expect(res[plan3_provider2.id][:price]).to eq 10000
+        end
+      end
+
+      context '複数(3)段階の価格表の場合' do
+        before do
+          plan1_rate_0_120
+          plan1_rate_121_300
+          plan1_rate_301
+        end
+
+        context '1段階目の場合' do
+          it '最小値の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(1)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 19.88
+          end
+
+          it '最大値の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(120)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 2385.6
+          end
+        end
+
+        context '2段階目の場合' do
+          it '最小値の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(121)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 2385.6 + 26.48
+          end
+
+          it '最大値の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(300)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 2385.6 + 4766.4
+          end
+        end
+
+        context '3段階目の場合' do
+          it '最小値の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(301)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 2385.6 + 4766.4 + 30.57
+          end
+
+          it '最小値以上の場合、価格計算が妥当であること' do
+            res = MeasuredRate.calc_prices(500)
+
+            expect(res.size).to eq 1
+            expect(res[plan1_provider1.id][:price]).to eq 2385.6 + 4766.4 + 6114
+          end
+        end
+      end
+
+      context '複数のプランがある場合' do
+        before do
+          plan1_rate_0_120.update!(price: 20.0)
+          plan1_rate_121_300.update!(price: 30.0)
+          plan1_rate_301.update!(price: 40.0)
+          plan2_rate_0_120
+          plan2_rate_121_300
+          plan2_rate_301
+          plan3_rate_0
+        end
+
+        it '使用量の条件に合致する複数のプランが集計されること' do
+          res = MeasuredRate.calc_prices(301)
+
+          expect(res.size).to eq 3
+          expect(res[plan1_provider1.id][:price]).to eq 7840
+          expect(res[plan2_provider1.id][:price]).to eq 4830
+          expect(res[plan3_provider2.id][:price]).to eq 3010
+        end
+      end
+    end
+  end
 end
