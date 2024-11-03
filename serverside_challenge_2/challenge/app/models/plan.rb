aa7a79c37b568667cf48a19e61d6ce47a7cf7226
kuni <kuni@kuninoMac-mini.local>
2024-11-03 12:07:25 +0900

    プラン事の基本料金、従量料金の計算するmethodを実装

--- serverside_challenge_2/challenge/app/models/plan.rb
+++ serverside_challenge_2/challenge/app/models/plan.rb
@@ -7,4 +7,42 @@ class Plan < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 1, maximum: 255 }
   validates :provider, presence: true, uniqueness: { scope: :name }
+
+  class << self
+    def calc_prices(amperage, electricity_usage_kwh)
+      errors = check_parameters(amperage, electricity_usage_kwh)
+      if errors.present?
+        return { errors: {
+          message: "リクエストパラメーターが正しくありません。",
+          details: errors
+        } }
+      end
+
+      plans = Plan.all.includes(:provider)
+      basic_prices_hash = BasicPrice.calc_prices(amperage)
+      measured_rates_hash = MeasuredRate.calc_prices(electricity_usage_kwh)
+      response = plans.map do |plan|
+        basic_price = basic_prices_hash[plan.id]
+        next if basic_price.nil?
+
+        measured_rate_price = measured_rates_hash[plan.id]&.[](:price) || 0
+        price = (basic_price + measured_rate_price).floor
+        {
+          plan: plan,
+          provider: plan.provider,
+          price: price
+        }
+      end
+      { plans: response }
+    end
+
+    def check_parameters(amperage, electricity_usage_kwh)
+      errors = [
+        BasicPrice.check_amperage?(amperage),
+        MeasuredRate.validate_electricity_usage?(electricity_usage_kwh)
+      ]
+      errors.select { |error| error[:is_error] }
+            .map { |error| error[:error_object] }
+    end
+  end
 end
--- serverside_challenge_2/challenge/spec/models/plan_spec.rb
+++ serverside_challenge_2/challenge/spec/models/plan_spec.rb
@@ -108,4 +108,91 @@ RSpec.describe Plan, type: :model do
       end
     end
   end
+
+  describe 'Methods' do
+    describe 'check_parameters' do
+      context 'amperage' do
+        it '正常な場合、レスポンスにエラーが含まれないこと' do
+          expect(Plan.check_parameters(20, 1000)).to be_empty
+        end
+
+        it 'エラーの場合、レスポンスにエラーが含まれること' do
+          res = Plan.check_parameters(0, 1000)
+          expect(res.size).to eq 1
+          expect(res[0][:field]).to eq 'amperage'
+          expect(res[0][:message]).to eq "#{BasicPrice::AMPERAGE_LIST.join('/')}のいずれかを指定してください。"
+        end
+      end
+
+      context 'electricity_usage_kwh' do
+        it 'エラーの場合、レスポンスにエラーが含まれること' do
+          res = Plan.check_parameters(20, nil)
+          expect(res.size).to eq 1
+          expect(res[0][:field]).to eq 'electricity_usage_kwh'
+          expect(res[0][:message]).to eq '整数を指定してください。'
+        end
+      end
+    end
+
+    describe 'calc_prices' do
+      let(:provider1) { create(:provider, name: 'provider1') }
+      let(:provider2) { create(:provider, name: 'provider2') }
+      # plan1
+      let(:plan1_provider1) { create(:plan, name: 'plan1', provider: provider1) }
+      let(:plan1_amp_10) { create(:basic_price, plan: plan1_provider1, amperage: 10, price: 100.01) }
+      let(:plan1_rate_0) { create(:measured_rate, plan: plan1_provider1, electricity_usage_min: 0, electricity_usage_max: nil, price: 10.01) }
+      # plan2
+      let(:plan2_provider2) { create(:plan, name: 'plan2', provider: provider2) }
+      let(:plan2_amp_10) { create(:basic_price, plan: plan2_provider2, amperage: 10, price: 200.01) }
+      let(:plan2_rate_0) { create(:measured_rate, plan: plan2_provider2, electricity_usage_min: 0, electricity_usage_max: nil, price: 20.03) }
+
+      before do
+        plan1_amp_10
+        plan1_rate_0
+        plan2_amp_10
+        plan2_rate_0
+      end
+
+      context 'エラーの場合' do
+        it 'レスポンスにエラーが設定されること' do
+          res = Plan.calc_prices(0, 1)
+          expect(res[:errors][:message]).to eq 'リクエストパラメーターが正しくありません。'
+          expect(res[:errors][:details].size).to eq 1
+          expect(res[:errors][:details][0][:field]).to eq 'amperage'
+          expect(res[:errors][:details][0][:message]).to include "のいずれかを指定してください。"
+        end
+      end
+
+      context '正常な場合' do
+        it 'プランの料金が集計されること' do
+          res = Plan.calc_prices(10, 1)
+
+          expect(res[:errors]).to be_nil
+          expect(res[:plans].size).to eq 2
+
+          plans = res[:plans]
+          expect(plans[0][:plan]).to eq plan1_provider1
+          expect(plans[0][:provider]).to eq provider1
+          expect(plans[0][:price]).to eq 110
+
+          expect(plans[1][:plan]).to eq plan2_provider2
+          expect(plans[1][:provider]).to eq provider2
+          expect(plans[1][:price]).to eq 220
+        end
+
+        it '存在しないプランを指定した場合、レスポンスに含まれないこと' do
+          plan2_amp_10.update!(amperage: 20)
+          res = Plan.calc_prices(10, 1)
+
+          expect(res[:errors]).to be_nil
+          expect(res[:plans].size).to eq 2
+
+          plans = res[:plans]
+          expect(plans[0][:plan]).to eq plan1_provider1
+          expect(plans[0][:provider]).to eq provider1
+          expect(plans[0][:price]).to eq 110
+        end
+      end
+    end
+  end
 end
