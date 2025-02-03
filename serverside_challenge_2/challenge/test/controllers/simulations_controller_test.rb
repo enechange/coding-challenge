2fa5adcc411bf248c018afdf048b8c16968258cf
Daiki Yano <daikitech0123@gmail.com>
2025-02-03 18:51:12 +0900

    specを採用したためtestフォルダは削除した

--- serverside_challenge_2/challenge/test/controllers/simulations_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class SimulationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get calculate" do
-    get simulations_calculate_url
-    assert_response :success
-  end
-end
