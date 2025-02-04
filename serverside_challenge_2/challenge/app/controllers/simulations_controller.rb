1ac975aed49fc303bd70e28a5fa23830cead5010
Daiki Yano <daikitech0123@gmail.com>
2025-02-04 21:25:40 +0900

    readme更新

--- serverside_challenge_2/challenge/app/controllers/simulations_controller.rb
+++ serverside_challenge_2/challenge/app/controllers/simulations_controller.rb
@@ -4,7 +4,7 @@ class SimulationsController < ApplicationController
     ampere = params[:ampere].to_i
     usage_kwh = params[:usage].to_i
     unless BasicRate::VALID_AMPERES.include?(ampere) && usage_kwh >= UsageRate::MIN_USAGE_KWH
-      return render json: { error: 'リクエストが不正です' }, status: :bad_request
+      return render json: { error: 'アンペアと使用量の値が不正です' }, status: :bad_request
     end
 
     result = Plan.simulate(ampere, usage_kwh)
