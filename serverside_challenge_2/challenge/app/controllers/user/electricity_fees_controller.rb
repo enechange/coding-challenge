c3295776367f7d0b8486b7d5455e0608933505a0
tontoko <hirakitomohiko@gmail.com>
2025-02-09 15:25:44 +0900

    fix: 入力パラメータが不足している場合のエラーハンドリングを追加

--- serverside_challenge_2/challenge/app/controllers/user/electricity_fees_controller.rb
+++ serverside_challenge_2/challenge/app/controllers/user/electricity_fees_controller.rb
@@ -6,6 +6,11 @@ module User
       contract_ampere = params[:contract_ampere].to_i
       usage_kwh = params[:usage_kwh].to_i
 
+      if params[:contract_ampere].blank? || params[:usage_kwh].blank?
+        render json: { error: 'Both contract_ampere and usage_kwh are required' }, status: :bad_request
+        return
+      end
+
       if contract_ampere.negative? || usage_kwh.negative?
         render json: { error: 'Invalid input' }, status: :bad_request
         return
--- serverside_challenge_2/challenge/spec/controllers/user/electricity_fees_controller_spec.rb
+++ serverside_challenge_2/challenge/spec/controllers/user/electricity_fees_controller_spec.rb
@@ -8,16 +8,18 @@ RSpec.describe User::ElectricityFeesController do
   describe 'GET #index' do
     let(:provider) { create(:electricity_provider, name: 'Test Provider') }
     let(:plan) { create(:electricity_plan, electricity_provider: provider, name: 'Test Plan') }
-    let(:params) { { contract_ampere: 30, usage_kwh: usage_kwh } }
+    let(:params) { { contract_ampere: contract_ampere, usage_kwh: usage_kwh } }
 
     before do
       create(:electricity_plan_basic_fee, electricity_plan: plan, ampere: 30, fee: 1000)
+      create(:electricity_plan_basic_fee, electricity_plan: plan, ampere: 40, fee: 2000)
       create(:electricity_plan_usage_fee, electricity_plan: plan, min_usage: 0, fee: 10)
       create(:electricity_plan_usage_fee, electricity_plan: plan, min_usage: 150, fee: 15)
     end
 
     context 'when usage is 100 kWh' do
       let(:usage_kwh) { 100 }
+      let(:contract_ampere) { 30 }
 
       it 'returns correct fee calculation' do
         subject
@@ -33,6 +35,7 @@ RSpec.describe User::ElectricityFeesController do
 
     context 'when usage is 200 kWh' do
       let(:usage_kwh) { 200 }
+      let(:contract_ampere) { 30 }
 
       it 'returns correct fee calculation' do
         subject
@@ -46,12 +49,39 @@ RSpec.describe User::ElectricityFeesController do
       end
     end
 
+    context 'when contract_ampere is different' do
+      let(:usage_kwh) { 100 }
+      let(:contract_ampere) { 40 }
+
+      it 'returns correct fee calculation' do
+        subject
+        expect(response).to have_http_status(:ok)
+        json_response = response.parsed_body
+        expect(json_response.first).to include(
+          'provider_name' => 'Test Provider',
+          'plan_name' => 'Test Plan',
+          'price' => '3000.0' # 2000 (basic fee) + 1000 (usage fee)
+        )
+      end
+    end
+
     context 'when input is invalid' do
       let(:params) { { contract_ampere: 0, usage_kwh: -1 } }
 
       it 'returns error' do
         subject
         expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'when params are missing' do
+      let(:params) { { contract_ampere: nil, usage_kwh: nil } }
+
+      it 'returns error' do
+        subject
+        expect(response).to have_http_status(:bad_request)
+        json_response = response.parsed_body
+        expect(json_response['error']).to eq('Both contract_ampere and usage_kwh are required')
       end
     end
   end
